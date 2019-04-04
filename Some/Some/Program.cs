db6f8a14ab698a3aa47f10ea26b2955833fae0a1
Kenny.McFarland <mcfarland.kenny@yahoo.com>
2019-04-03 20:51:44 -0500

    What Is Happening

---
+++ Some/Some/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Some
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintToScreen();
+        }
+        public static void PrintToScreen()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
